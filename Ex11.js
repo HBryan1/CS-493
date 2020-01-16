917815796b9ed079835ee54a816c99cd6cabe4cd
HBryan1 <59447508+HBryan1@users.noreply.github.com>
2020-01-15 21:23:58 -0500

    Update Ex11.js

--- Ex11.js
+++ Ex11.js
@@ -1,4 +1,3 @@
-// JavaScript source code
 let showText = document.getElementsByClassName("btn")[0];
 showText.onclick = function () {
     document.getElementById("leadtext").innerText = "I love Coding";
@@ -7,7 +6,7 @@ showText.onclick = function () {
 let addMenu = document.getElementsByClassName("btn")[1];
 addMenu.onclick = function () {
     let node = document.createElement("li");
-    node.innerHTML = "<li class='nav-link'>New Menu</li>";
+    node.innerHTML = "<a class='nav-link'>New Menu</a>";
     node.style.color = "blue";
     document.getElementsByClassName("nav")[0].appendChild(node);
 }
@@ -20,10 +19,29 @@ removeMenu.onclick = function () {
 
 let changeImage = document.getElementsByClassName("btn")[3];
 changeImage.onclick = function () {
-    document.getElementsByClassName("float-right").innerHTML = "<img class='float - right' src='img.jpg' style='width: 100px;'/>"
+      document.getElementsByClassName("float-right").src ="images/linkedin.png";
 }
 
 let showAlert = document.getElementsByClassName("btn")[4];
 showAlert.onclick = function () {
+  if( document.getElementById("alertbox").innerHTML == ""){
     document.getElementById("alertbox").innerHTML = "<div class='alert alert-success'> Some Text </div>";
+    showAlert.innerText = "Remove Alert";
+  }
+  else{
+    document.getElementById("alertbox").innerHTML = "";
+    showAlert.innerText = "Show Alert";
+  }
+}
+
+let add = document.getElementById("btn-new-task");
+add.onclick = function() {
+  let task = document.getElementById("new-task").value;
+  let node = document.createElement("li");
+  node.innerHTML = "<input type='checkbox'> <label> </label><button class='delete'>X</button>";
+  document.getElementById("todo-tasks").appendChild(node);
+}
+let removeList = document.getElementsByClassName("delete");
+removeList.onclick = function(){
+  
 }
