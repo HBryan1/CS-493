5dd8f66a0dd7135576b4fc2ce8dc759f1f1936c5
HBryan1 <59447508+HBryan1@users.noreply.github.com>
2020-03-30 18:20:44 -0400

    Update drawing.js

--- Drawing/drawing.js
+++ Drawing/drawing.js
@@ -1,46 +1,86 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init(){
+    let colors = "black";
+    let lineSize = 1;
+    let btnRed = document.getElementById("red");
+    let btnGreen = document.getElementById("green");
+    let btnBlue = document.getElementById("blue");
+    let btnBlack = document.getElementById("black");
+    let btnSm = document.getElementById("sm");
+    let btnLg = document.getElementById("lg");
+    let btnErase = document.getElementById("erase");
+
+    btnErase.onclick = function (){ colors = "#f8f8f8"}
+    btnSm.onclick = function (){ lineSize = 1}
+    btnLg.onclick = function (){ lineSize = 6}
+    btnRed.onclick = function (){ colors = "red"}
+    btnGreen.onclick = function (){ colors = "green"}
+    btnBlue.onclick = function (){ colors = "blue"}
+    btnBlack.onclick = function (){ colors = "black"}
+
     let xpos;
     let ypos;
-    let prevX = 0,
-        currX = 0,
-        prevY = 0,
-        currY = 0,
+    let prevX = 0;
+    let currX = 0;
+    let prevY = 0;
+    let currY = 0;
+
     context = document.getElementById("canvasID").getContext("2d");
     canvas = document.getElementById("canvasID");
-    context.beginPath();
-    context.strokeRect(50, 35, 50, 50);
-    
-    context.beginPath();
-    context.fillRect(125, 35, 50, 50);
-    
-    context.beginPath();
-    context.strokeStyle = "red";
-    context.fillStyle = "blue";
-    context.lineWidth = 5;
-    context.rect(200, 35, 50, 50);
-    context.fill();
-    context.stroke();
-    
+
+    canvas.addEventListener("mousemove", function (e) {
+            findxy('move', e)
+        }, false);
+        canvas.addEventListener("mousedown", function (e) {
+            findxy('down', e)
+        }, false);
+        canvas.addEventListener("mouseup", function (e) {
+            findxy('up', e)
+        }, false);
+        canvas.addEventListener("mouseout", function (e) {
+            findxy('out', e)
+        }, false);
+
     function draw() {
         context.beginPath();
         context.moveTo(prevX, prevY);
         context.lineTo(currX, currY);
-        context.strokeStyle = "blue";
-        context.lineWidth = 5;
+        context.strokeStyle = colors;
+        context.lineWidth = lineSize;
         context.stroke();
         context.closePath();
     }
-    
-    canvas.onclick = function(e){
-        prevX = currX;
-        prevY = currY;
-        currX = e.clientX - canvas.offsetLeft;
-        currY = e.clientY - canvas.offsetTop;
-        
-        draw();
-        return xpos;
-    }
-    
+    function findxy(res, e) {
+            if (res == 'down') {
+                prevX = currX;
+                prevY = currY;
+                currX = e.clientX - canvas.offsetLeft;
+                currY = e.clientY - canvas.offsetTop;
+
+                flag = true;
+                dot_flag = true;
+                if (dot_flag) {
+                    ctx.beginPath();
+                    ctx.fillStyle = x;
+                    ctx.fillRect(currX, currY, 2, 2);
+                    ctx.closePath();
+                    dot_flag = false;
+                }
+            }
+            if (res == 'up' || res == "out") {
+                flag = false;
+            }
+            if (res == 'move') {
+                if (flag) {
+                    prevX = currX;
+                    prevY = currY;
+                    currX = e.clientX - canvas.offsetLeft;
+                    currY = e.clientY - canvas.offsetTop;
+                    draw();
+                }
+            }
+        }
+
+
 }
